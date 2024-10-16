fc3b0fa3fabe2c2c0be6f65aab55eb788114d4fa
Trent Pfeiffer <trentpf@hotmail.com>
2024-10-15 20:18:14 -0400

    Add tests for Task 2

--- REQ_001/REQ_001.cpp
+++ REQ_001/REQ_001.cpp
@@ -3,6 +3,8 @@
 
 extern "C" int getPerimeter(int* length, int* width);
 extern "C" int getArea(int* length, int* width);
+extern "C" void setLength(int input, int* length);
+extern "C" void setWidth(int input, int* width);
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -27,7 +29,6 @@ namespace REQ001
 			Result = getPerimeter(ptrlength, ptrwidth);
 
 			Assert::AreEqual(30, Result);
-
 		}
 
 		TEST_METHOD(GetAreaFunctionality)
@@ -47,6 +48,84 @@ namespace REQ001
 			Assert::AreEqual(50, Result);
 		}
 
+		TEST_METHOD(SetLengthFunctionality)
+		{
+			//This is testing the setLength function using a valid length 5
 
+			int temp = -1;
+			int length = 5;
+			int* ptrlength = &temp;
+
+			setLength(length, ptrlength);
+
+			Assert::AreEqual(length, *ptrlength);
+		}
+
+		TEST_METHOD(SetLengthTo100)
+		{
+			//This is testing the setLength function to see if a value is not set when the value is 100
+
+			int temp = -1;
+			int length = 100;
+			int* ptrlength = &temp;
+
+			setLength(length, ptrlength);
+
+			Assert::AreNotEqual(length, *ptrlength);
+		}
+
+		TEST_METHOD(SetLengthTo0)
+		{
+			//This is testing the setLength function to see if a value is not set when the value is 0
+
+			int temp = -1;
+			int length = 0;
+			int* ptrlength = &temp;
+
+			setLength(length, ptrlength);
+
+			Assert::AreNotEqual(length, *ptrlength);
+		}
+
+		TEST_METHOD(SetWidthFunctionality)
+		{
+			//This is testing the setWidth function using a valid width 10
+
+			int temp = -1;
+			int width = 10;
+			int* ptrwidth = &temp;
+
+			setWidth(width, ptrwidth);
+
+			Assert::AreEqual(width, *ptrwidth);
+		}
+
+		TEST_METHOD(SetWidthTo100)
+		{
+			//This is testing the setWidth function to see if a value is not set when the value is 100
+
+			int temp = -1;
+			int width = 100;
+			int* ptrwidth = &temp;
+
+			setWidth(width, ptrwidth);
+
+			Assert::AreNotEqual(width, *ptrwidth);
+		}
+
+
+		TEST_METHOD(SetWidthTo0)
+		{
+			// This is testing the setWidth function to see if a value is not set when the value is 0
+
+			int temp = -1;
+			int width = 0;
+			int* ptrwidth = &temp;
+
+			setWidth(width, ptrwidth);
+
+			Assert::AreNotEqual(width, *ptrwidth);
+		}
+		
 	};
 }
